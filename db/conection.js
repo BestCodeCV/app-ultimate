4a5ec6687fceb9770e853a8944d9c9a2f4f8fed8
BestCode <bestcodecv@gmail.com>
2021-06-15 16:08:01 -0500

    nuevo y mejor

--- db/conection.js
+++ db/conection.js
@@ -1,9 +1,9 @@
 let mysql = require('mysql')
 let con = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'asistente'
+    user: 'id16513207_user_database',
+    password: 'pAsS_admin_2021',
+    database: 'id16513207_asistente'
 })
 con.connect()
 module.exports = con;
